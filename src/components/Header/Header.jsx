c4f28f943e2ce24cbe347193f86f46af85c9b199
Zarva <zarva6596@gmail.com>
2020-10-19 13:51:21 +0300

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export const Header = ({ user, changeUser }) => (
       className="header__logo-link"
     >
       <img
-        src="/images/x.png"
+        src="/images/logo.png"
         className="header__logo"
         alt="Logo"
       />
